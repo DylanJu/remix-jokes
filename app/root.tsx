c3a296fadfba70d1fb9790774df7d8e004f452b5
DylanJu <dylan@daangnpay.com>
2022-10-10 23:35:06 +0900

    tutorial-2: database with prisma

--- app/root.tsx
+++ app/root.tsx
@@ -8,23 +8,64 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
+import type { LinksFunction } from "@remix-run/node";
+
+import globalStylesUrl from "./styles/global.css";
+import globalMediumStylesUrl from "./styles/global-medium.css";
+import globalLargeStylesUrl from "./styles/global-large.css";
+
+// export const meta: MetaFunction = () => ({
+//   charset: "utf-8",
+//   title: "New Remix App",
+//   viewport: "width=device-width,initial-scale=1",
+// });
+
+// export default function App() {
+//   return (
+//     <html lang="en">
+//       <head>
+//         <Meta />
+//         <Links />
+//       </head>
+//       <body>
+//         <Outlet />
+//         <ScrollRestoration />
+//         <Scripts />
+//         <LiveReload />
+//       </body>
+//     </html>
+//   );
+// }
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: globalStylesUrl,
+    },
+    {
+      rel: "stylesheet",
+      href: globalMediumStylesUrl,
+      media: "print, (min-width: 640px)",
+    },
+    {
+      rel: "stylesheet",
+      href: globalLargeStylesUrl,
+      media: "screen and (min-width: 1024px)",
+    },
+  ];
+};
 
 export default function App() {
   return (
     <html lang="en">
       <head>
-        <Meta />
+        <meta charSet="utf-8" />
+        <title>Remix: So great, it's funny!</title>
         <Links />
       </head>
       <body>
         <Outlet />
-        <ScrollRestoration />
-        <Scripts />
         <LiveReload />
       </body>
     </html>
